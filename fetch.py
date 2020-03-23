8f6a710e5edb69c090d2ba746af26d3c80fcabbf
saifbilal <saif.bilal.81@gmail.com>
2020-03-23 15:31:21 +0500

    added latest file tracing

--- fetch.py
+++ fetch.py
@@ -1,5 +1,5 @@
 import paramiko
-
+import os
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())   # This script doesn't work for me unless this line is added!
 ssh.connect("192.168.2.219", port=22, username="root", password="nokia123",timeout=5000)
@@ -7,3 +7,10 @@ stdin, stdout, stderr = ssh.exec_command("ls")
 opt = stdout.readlines()
 opt = "".join(opt)
 print(opt)
+list_of_files = glob.glob('/home/stack/healthchecksbyomar/*')
+latest_file = max(list_of_files, key=os.path.getctime)
+print latest_file
+#copy files from remote host to local host
+ftp_client=ssh.open_sftp()
+#ftp_client.get('/home/stack/healthchecksbyomar/hchkomar_20200322','/home/stack/hchkomar_20200322')
+ftp_client.close()
